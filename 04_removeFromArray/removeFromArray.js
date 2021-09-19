730b9c36c296bb277372e2140ebef27e611f4f16
Zachary Truong <zacharytruong@gmail.com>
2021-09-19 11:46:14 -0500

    Finished 01 - 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,11 @@
 const removeFromArray = function(arr1, num0, num1 = '', num2 = '', num3 = '') {
     const arr2 = [num0, num1, num2, num3];
-    arr2.forEach(compareArray);
-    function compareArray(arr2Value){
+    arr2.forEach(compareValues);
+    function compareValues(arr2Value){
         arr1.forEach(removeValue);
         function removeValue(arr1Value, arr1Index){
-            if (arr2Value === arr1Value){
-                arr1.splice(arr1Index, 1);
-            }
-        }    
+            if (arr1Value === arr2Value) arr1.splice(arr1Index, 1);
+        }
     }
     return arr1;
 }
